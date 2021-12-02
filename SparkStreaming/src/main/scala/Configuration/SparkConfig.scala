e7460c81f134b88e3c9dbadedc09c80e6449b568
Gnanamanickam A <gnanamanickam1995@gmail.com>
2021-12-01 22:06:37 -0600

    Method to Read kafka

--- SparkStreaming/src/main/scala/Configuration/SparkConfig.scala
+++ SparkStreaming/src/main/scala/Configuration/SparkConfig.scala
@@ -1,16 +1,22 @@
 package Configuration
 
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 trait SparkConfig {
+
+  // create a spark conf without spark session
+//  val conf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming")
+
   val spark: SparkSession = SparkSession
     .builder()
-    .appName("Spark Streaming")
-    .master("local[*]")
-    .getOrCreate()
+    .appName("SparkStreaming") // appName parameter is a name for the application to show on the cluster UI
+    .master("local[*]") // master can be Spark Mesos or YARN cluster URL or local for local mode . * represents no. of threads
+    .getOrCreate() // get if available already or create a new one
 
+  //  Only one SparkContext should be active per JVM. Stop the active SparkContext before creating a new one.
   val sparkContext: SparkContext = spark.sparkContext
+  // Create a StreamingContext with working thread and batch interval of 10 seconds.
   val streamingContext: StreamingContext = new StreamingContext(sparkContext, Seconds(10))
 }
--- SparkStreaming/src/main/scala/SparkStreaming.scala
+++ SparkStreaming/src/main/scala/SparkStreaming.scala
@@ -1,3 +1,29 @@
-class SparkStreaming {
+import Configuration.{KafkaConfig, SparkConfig}
+import com.typesafe.config.ConfigFactory
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.SparkContext
+
+object SparkStreaming extends App with SparkConfig with KafkaConfig {
+
+  // Load the config file from resources path
+  val config = ConfigFactory.load("application.conf")
+
+  // subscribe to topic
+  val streamDF = spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", config.getString("sparkStreaming.kafkaServer"))
+    .option("subscribe", "topic")
+    .load()
 
 }
