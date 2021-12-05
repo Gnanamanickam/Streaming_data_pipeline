cc6f13639b19ca0043556f03075b50ccde84b81e
Gnanamanickam A <gnanamanickam1995@gmail.com>
2021-12-04 21:18:50 -0600

    Added Word Count file

--- SparkStreaming/src/test/scala/SparkStreamingTest.scala
+++ SparkStreaming/src/test/scala/SparkStreamingTest.scala
@@ -1,9 +1,19 @@
 
+import Utils.WordCount
 import com.typesafe.config.ConfigFactory
+import org.scalatest.BeforeAndAfter
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.{Seconds, Time}
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Span}
+import org.scalatest.GivenWhenThen
 
-class SparkStreamingTest extends AnyFlatSpec with Matchers {
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+class SparkStreamingTest extends AnyFlatSpec with BeforeAndAfter with GivenWhenThen with Matchers with Eventually {
 
   // Get the config values from application.conf in resources
   val config = ConfigFactory.load("Application.conf").getConfig("randomLogGenerator")
@@ -12,10 +22,19 @@ class SparkStreamingTest extends AnyFlatSpec with Matchers {
 
   behavior of "Configuration Parameters Module"
 
-  // check whether a random generated string length is lesser than the minimum length
+  // check whether the kafka server address is right
   it should "Check for the kafka server" in {
     kafkaServer shouldBe >= ("127.0.0.1:9092")
   }
 
+  "Sample set" should "be counted" in {
+    Given("streaming context is initialized")
+    val lines = mutable.Queue[RDD[String]]()
+    var results = ListBuffer.empty[Array[WordCount]]
+
+//    WordCount.count(s,lines) {
+//      (wordsCount: RDD[WordCount], time: Time) =>
+//      results += wordsCount.collect()
+//  }
 
 }
